9b23439cfb90dc242d727c609cc5351206fb3e99
高强 <gaoqiang1112@163.com>
2019-12-09 09:32:15 +0800

    1

--- src/diff.js
+++ src/diff.js
@@ -3,7 +3,7 @@
 // 1 当节点类型相同时，去看一下属性是否相同 产生一个属性的补丁包 {type:'ATTRS',attrs:{class:'list-group'}}
 // 2 新的dom节点不存在 {type:'REMOVE',index:xxxxx}
 // 3 节点类型不相同 直接采用替换模式 {type:'REPLACE',newNode:newNode}
-//
+// 4
 function diff() {
     let patches = {}
 
